8a69245e0d402d3113ffd59bbb132abb7cc9338a
Henrique Pesciotti <hpesciotti@gmail.com>
2024-09-21 12:51:00 +0000

    fix/chore: create cave relation table and install django all auth

--- caves/admin.py
+++ caves/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Cave
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
-admin.site.register(Cave)
+@admin.register(Cave)
+class CaveAdmin(SummernoteModelAdmin):
+
+    list_display = ('cave_name', 'author', 'geomorph_unit')
+    search_fields = ['cave_name', 'author']
+    # prepopulated_fields = {'slug': ('cave_name',)}
+    summernote_fields = ('description',)
