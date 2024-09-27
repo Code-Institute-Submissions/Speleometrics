0a44ed082f6142ab22610e5c54059086d76eff82
Henrique Pesciotti <hpesciotti@gmail.com>
2024-09-27 19:01:10 +0000

    fix: user types in profiles

--- profiles/models.py
+++ profiles/models.py
@@ -1,12 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+USER_TYPE = (
+    (0, "Academic"),
+    (2, "Company"),
+    (3, "Professional Speleologist"),
+    (4, "Speleology Enthusiast"),
+    (5, "Governmental Agency"),
+)
 
 class Profile(models.Model):
     """
     Add profile info for the user.
     """
-    email_contact = models.EmailField(max_length=100)
+    email_contact = models.EmailField(max_length=100, blank=False)
     profile_type = models.IntegerField(choices=USER_TYPE, 
         default=0, blank=False)
     bio = models.TextField(blank=False)
--- speleometrics/settings.py
+++ speleometrics/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'django_summernote',
     'caves',
+    'profiles',
 ]
 
 SITE_ID = 1
