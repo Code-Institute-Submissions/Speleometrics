b09ef2d5e637a1140413a4615248a1352ca743ff
Henrique Pesciotti <hpesciotti@gmail.com>
2024-09-29 20:48:51 +0000

    chore: add one-to-one relationship between user and profile

--- profiles/admin.py
+++ profiles/admin.py
@@ -3,10 +3,9 @@ from .models import Profile
 from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
-
+@admin.register(Profile)
 class ProfileAdmin(SummernoteModelAdmin):
 
     list_display = ('user', 'bio')
     search_fields = ['profile_type', 'user']
-    # prepopulated_fields = {'slug': ('cave_name',)}
     summernote_fields = ('bio',)
--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -19,7 +21,16 @@ class Profile(models.Model):
     profile_type = models.IntegerField(choices=USER_TYPE, 
         default=0, blank=False)
     bio = models.TextField(blank=False)
-    user = models.ForeignKey(
+    user = models.OneToOneField(
         User, on_delete=models.CASCADE,
         related_name="user_profile"
     )
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
