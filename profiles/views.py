d0518c9633e8757248f50a61d8f3851edddf89fa
Henrique Pesciotti <hpesciotti@gmail.com>
2024-09-30 09:50:40 +0000

    fix: refactor view and edit profile functions applying signals to create profile when a user is created

--- profiles/views.py
+++ profiles/views.py
@@ -8,11 +8,11 @@ from .forms import ProfileForm
 @login_required
 def view_profile(request):
     """ 
-    Returns user profile 
+    Returns user profile, if contact email is empity return edit profile form.
     """
-    try:
-        profile = Profile.objects.get(user=request.user)
-    except Profile.DoesNotExist:
+    profile = get_object_or_404(Profile, user=request.user)
+    
+    if not profile.email_for_contact:
         return redirect('profile-edit')
     return render(request, 'profiles/profile_page.html', {'profile': profile})
 
@@ -21,12 +21,13 @@ def edit_profile(request):
     """ 
     Edit Profile
     """
-    def view_profile(request):
-        form = ProfileForm(request.POST)
-        if form.is_valid():
-            profile = form.save(commit=False)
-            profile.user = request.user
-            profile.save()
-        return redirect('profile')
-    return render(request, 'profiles/profile_edit.html', {'form': form})
+    profile = get_object_or_404(Profile, user=request.user)
 
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = ProfileForm(instance=profile)
+    return render(request, 'profiles/profile_edit.html', {'form': form})
