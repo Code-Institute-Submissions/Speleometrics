34c659fa9bdf06d775f5a64577ef5783d7088f50
Henrique Pesciotti <hpesciotti@gmail.com>
2024-10-06 13:43:48 +0000

    chore: add cave/edit view to cave views

--- caves/urls.py
+++ caves/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.cave_list_table, name='home'),
-    path('cave/<str:cave_name>', views.cave_page, name='cave_page'),
+    path('cave/<str:cave_name>/', views.cave_page, name='cave_page'),
+    path('cave/<str:cave_name>/edit/<str:username>', views.edit_cave, name='edit_cave')
 ]
