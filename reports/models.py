2cc5fb8d492ada9d1b7724d3e789da82850e5ca9
Henrique Pesciotti <hpesciotti@gmail.com>
2024-10-08 09:17:40 +0000

    chore: add model Report

--- reports/models.py
+++ reports/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
+from caves.models import Cave
 
 # Create your models here.
+
+class Report(models.Model):
+    reported_by = models.ForeignKey(User, related_name='reports_made', on_delete=models.CASCADE)
+    cave = models.ForeignKey(Cave, related_name='reports_cave', on_delete=models.CASCADE)
+    cave_owner = models.ForeignKey(User, related_name='reports_made', on_delete=models.CASCADE)
+    inconsistency_details = models.TextField()
+    reported_created_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'{ self.cave.cave_name }, owned by { 
+            self.cave.cave_owner.username } reported by {
+              self.reported_by.username  
+            }'
