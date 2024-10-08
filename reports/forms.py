e47dae344ddce8c5508df0f1098f636d70bccfde
Henrique Pesciotti <hpesciotti@gmail.com>
2024-10-08 10:09:41 +0000

    chore: add reportform to reports

---
+++ reports/forms.py
@@ -0,0 +1,22 @@
+from djsngo import forms
+from .models import Report
+
+class ReportForm(forms.ModelForm):
+    """
+    Add a report message box to identify inconsistencies in the database.
+    This is done by a modal that will display a textbox.
+    """
+    class Meta:
+        model = Report
+        fields = ['inconsistency_details']
+        labels = {
+            'inconsistency_details': 
+            'Describe the inconsistency on the cave data'
+        }
+        widgets = {
+            'inconsistency_details': 
+            forms.Textarea(attrs={
+                'rows': 5,
+                'cols': 40
+            })
+        }
